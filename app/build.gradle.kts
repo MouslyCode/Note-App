e229d129d729c0e051d4fbbd37fefb09b4efefea
MouslyCode <rezasubkhan50@gmail.com>
2025-03-24 00:22:17 +0700

    fixing the error on google sign in

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
 
     defaultConfig {
         applicationId = "com.mouscode.noteapp"
-        minSdk = 24
+        minSdk = 34
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
