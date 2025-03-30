2fa230008774e9101fc24cfe3f4ed12756cfca34
MouslyCode <rezasubkhan50@gmail.com>
2025-03-30 23:06:00 +0700

    minor commit

--- app/src/main/java/com/mouscode/noteapp/feature/auth/presentation/SignUpScreen.kt
+++ app/src/main/java/com/mouscode/noteapp/feature/auth/presentation/SignUpScreen.kt
@@ -1,9 +1,24 @@
 package com.mouscode.noteapp.feature.auth.presentation
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.mouscode.noteapp.ui.theme.NoteAppTheme
 
 @Composable
 fun SignUpScreen(modifier: Modifier = Modifier) {
-    
+    NoteAppTheme {
+        Scaffold { paddingValues ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues = paddingValues)
+            ) {
+
+            }
+        }
+    }
 }
