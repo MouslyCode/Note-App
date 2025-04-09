2378cca835dd180f63f08f56f9f17dd11de23f7f
MouslyCode <rezasubkhan50@gmail.com>
2025-04-09 08:09:40 +0700

    Auth Repository

--- app/src/main/java/com/mouscode/noteapp/feature/auth/domain/repository/AuthRepository.kt
+++ app/src/main/java/com/mouscode/noteapp/feature/auth/domain/repository/AuthRepository.kt
@@ -1,10 +1,12 @@
 package com.mouscode.noteapp.feature.auth.domain.repository
 
 import com.mouscode.noteapp.feature.auth.data.repository.util.AuthResponse
+import com.mouscode.noteapp.feature.auth.domain.model.User
 
 interface AuthRepository {
     suspend fun createAccountWithEmail(username:String,email: String,password: String): AuthResponse
     suspend fun signInWithEmail(email: String, password: String): AuthResponse
     suspend fun signInWithGoogle(email: String, password: String): AuthResponse
     suspend fun signOut(): AuthResponse
+    suspend fun getUser() : User?
 }
